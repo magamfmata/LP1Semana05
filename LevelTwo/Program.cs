3c1b62008af2e1276360d5d15d55020400f1f4f7
Magali <magamayrmata@gmail.com>
2025-03-21 00:42:49 +0000

    JPG added 24 char

--- LevelTwo/Program.cs
+++ LevelTwo/Program.cs
@@ -5,14 +5,36 @@ namespace LevelTwo
 {
     public class Program
     {
-        private static void Main()
+        private static void Main(string[] args)
         {
+            if (args.Length == 0)
+            {
+
+
             CanvasImage image = new CanvasImage("tux.jpg");
 
-            image.MaxWidth(20);
+
+            image.MaxWidth(24);
 
             AnsiConsole.Write(image);
+            }
+            else
+            {
+                string inputImage = args[0];
 
+                string inputWidthStr = args[1];
+
+                CanvasImage image = new CanvasImage("tux.jpg");
+
+                int inputW = int.Parse(inputWidthStr);
+
+                image.MaxWidth(inputW);
+
+                AnsiConsole.Write(image);
+
+
+
+            }
         }
     }
 }
